184bb494aefea0d07f0e5aacea3740ee5d8102b1
jringo1 <josephringo76@yahoo.com>
2020-02-29 16:14:25 -0800

    One-page -website and pizza project

---
+++ One-Page Website/JS/one_page_website.js
@@ -0,0 +1,19 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
